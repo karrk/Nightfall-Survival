10d5e874f6620262eb40bc45b79326b7eb27189c
asterism1030 <rlaguswl1030@gmail.com>
2024-03-28 20:43:14 +0900

    #0.0.1 조이스틱 기능 구현
    - 기존의 VirtualJoystick 에셋, 스크립트 삭제
    - Joystick 기능 구현
    - 조이스틱의 bg 드래그 기능 활성화

--- Assets/02.Scripts/Graphic/Character/CharacterController.cs
+++ Assets/02.Scripts/Graphic/Character/CharacterController.cs
@@ -7,17 +7,15 @@ using static UnityEditor.PlayerSettings;
 
 public class CharacterController : MonoBehaviour
 {
-    // TODO) 임시 이므로 수정해야함
-    // 조이스틱 직접 구현한 것으로 교체
+    // TODO)
     // 캐릭터 이미지가 아닌 스프라이트로 교체 후 거리 맞추기
     [SerializeField]
-    private VirtualJoystick joystick;
+    private JoyStick joystick;
     //private JoyStick joyStick;
 
-    public float movementSpeed = 3.0f;
     ////////////////////////////////
+    public float movementSpeed = 3.0f;
     
-
     private Rigidbody2D rigidbody2D;
 
     void Start()
@@ -40,24 +38,11 @@ public class CharacterController : MonoBehaviour
 
     public void Move(Vector2 pos)
     {
-        // TODO) 수정 예정
-        /*
-        float dirX = pos.x;
-        float dirY = pos.y;
-
-        Vector3 moveX = transform.right * dirX;
-        Vector3 moveY = transform.up* dirY;
-
-        Vector3 velocity = (moveX + moveY).normalized * movementSpeed;
-
-        rigidbody2D.MovePosition(transform.position + velocity * Time.deltaTime);
-        */
         float x = pos.x;
         float y = pos.y;
 
         Vector3 velocity = new Vector3(x, y, 0);
         velocity *= movementSpeed;
         rigidbody2D.velocity = velocity;
-
     }
 }
