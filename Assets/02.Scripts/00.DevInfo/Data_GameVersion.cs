782726e1beae300beaa04629ffab94b87eb2da91
alld <hkh0992@naver.com>
2024-04-03 21:17:50 +0900

    #0.0.13
    - 기능 최종

--- Assets/02.Scripts/00.DevInfo/Data_GameVersion.cs
+++ Assets/02.Scripts/00.DevInfo/Data_GameVersion.cs
@@ -8,7 +8,7 @@ namespace VS.Version
         /// <summary>
         /// [버전] 게임에서 사용되어지는 통합 버전입니다. 
         /// </summary>
-        public const string Version = "0.0.11";
+        public const string Version = "0.0.13";
 
         /// <summary>
         /// [동진님] 
